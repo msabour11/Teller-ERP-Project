56189d45eddad42969d9c9c76e3dc82d5f5a686f
mohamed abdelsabour <83145597+msabour11@users.noreply.github.com>
2024-03-05 11:58:36 +0200

    add the count of last printed number

--- teller/teller_customization/doctype/printing_roll/printing_roll.js
+++ teller/teller_customization/doctype/printing_roll/printing_roll.js
@@ -1,3 +1,7 @@
 frappe.ui.form.on("Printing Roll", {
-  onload: function (frm) {},
+  refresh: function (frm) {
+    // frappe.msgprint("frm.doc.show_number");
+    last_num_str = frm.doc.last_printed_number.toString().length;
+    frm.set_value("show_number", last_num_str);
+  },
 });

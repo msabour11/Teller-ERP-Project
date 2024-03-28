58161ba33de68eda213422e7f6efa51375f94e3c
mohamed abdelsabour <83145597+msabour11@users.noreply.github.com>
2024-03-28 15:21:01 +0200

    add payment entry

---
+++ teller/teller_customization/doctype/sales_entry/sales_entry.js
@@ -0,0 +1,40 @@
+// Copyright (c) 2024, Mohamed AbdElsabour and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Sales Entry", {
+	refresh(frm) {
+
+	},
+});
+frappe.ui.form.on("Entry Child",{
+	paid_from:function (frm,cdt,cdn){
+		var row=locals[cdt][cdn]
+		if (row.paid_from)
+		{
+			frappe.call({
+				method:"teller.teller_customization.doctype.sales_entry.sales_entry.get_currency",
+				args:{
+					account:row.paid_from
+				},
+				callback:function (r){
+					console.log(r.message[0])
+					let curr=r.message[0]
+					let currency_rate=r.message[1]
+					frappe.model.set_value(cdt,cdn,"currency",curr)
+					frappe.model.set_value(cdt,cdn,"rate",currency_rate)
+					// frappe.call({
+					// 	method:"teller.teller_customization.doctype.sales_entry.create_payment_entry",
+					// 	args:{
+					// 		account:row.paid_from,
+					// 		usd_amount:row.usd_amount,
+					// 		recieved_amount:row.received_amount,
+					// 		paid_to:row.paid_to
+					//
+					// 	}
+					// })
+				}
+			})
+
+		}
+	}
+})
---
+++ teller/teller_customization/doctype/sales_entry/sales_entry.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2024, Mohamed AbdElsabour and contributors
+# For license information, please see license.txt
+
+import frappe
+from datetime import datetime
+from frappe.model.document import Document
+from frappe import _, utils
+
+
+
+class SalesEntry(Document):
+    def validate(self):
+        self.create_payment_entry()
+
+
+@frappe.whitelist(allow_guest=True)
+def get_currency(account):
+    # currency = frappe.db.get_value("Payment Entry",{"paid_from":account}, "paid_from_account_currency")
+    currency = frappe.db.get_value("Account", {"name": account}, "account_currency")
+    currency_rate = frappe.db.get_value("Currency Exchange", {"from_currency": currency}, "exchange_rate")
+    return currency, currency_rate
+
+@frappe.whitelist(allow_guest=True)
+def create_payment_entry(account, currency,usd_amount,recieved_amount,paid_to):
+    payment_entry = frappe.new_doc("Payment Entry")
+    payment_entry.payment_type = "Internal Transfer"
+    payment_entry.paid_from = account
+    payment_entry.paid_to = paid_to
+    payment_entry.paid_amount = usd_amount
+    # payment_entry.party_type = "Customer"
+    payment_entry.received_amount = recieved_amount
+    # payment_entry.party = "Customer Name"
+    payment_entry.reference_no = "Ref No"
+    payment_entry.reference_date = utils.getdate()
+    # payment_entry.mode_of_payment = "Cash"
+    payment_entry.save()

b089c86290e72a4448c0ea056d47c91006f50c3f
msabour11 <mohamedabdo2611@gmil.com>
2024-04-03 08:23:49 -0400

    get currency

--- teller/teller_customization/doctype/sales_entry/sales_entry.js
+++ teller/teller_customization/doctype/sales_entry/sales_entry.js
@@ -3,14 +3,15 @@
 
 frappe.ui.form.on("Sales Entry", {
 	refresh(frm) {
-		frappe.msgprint('test new bench')
+		 frappe.msgprint('test new bench')
+		 console.log("test")
 
 	},
 });
 frappe.ui.form.on("Entry Child",{
 	paid_from:function (frm,cdt,cdn){
 		var row=locals[cdt][cdn]
-		if (row.paid_from && row.usd_amount && row.received_amount && row.paid_to)
+		if (row.paid_from )
 		{
 			frappe.call({
 				method:"teller.teller_customization.doctype.sales_entry.sales_entry.get_currency",
@@ -18,25 +19,26 @@ frappe.ui.form.on("Entry Child",{
 					account:row.paid_from
 				},
 				callback:function (r){
+					console.log(r.message)
 					console.log(r.message[0])
 					let curr=r.message[0]
 					let currency_rate=r.message[1]
 					frappe.model.set_value(cdt,cdn,"currency",curr)
 					frappe.model.set_value(cdt,cdn,"rate",currency_rate)
-					frappe.call({
-						method:"teller.teller_customization.doctype.sales_entry.sales_entry.create_payment_entry",
-						args:{
-							account:row.paid_from,
-							usd_amount:row.usd_amount,
-							recieved_amount:row.received_amount,
-							paid_to:row.paid_to
+					// frappe.call({
+					// 	method:"teller.teller_customization.doctype.sales_entry.sales_entry.create_payment_entry",
+					// 	args:{
+					// 		account:row.paid_from,
+					// 		usd_amount:row.usd_amount,
+					// 		recieved_amount:row.received_amount,
+					// 		paid_to:row.paid_to
 
-						},
-						callback:function (r)
-						{
-							console.log(r.message)
-						}
-					})
+					// 	},
+					// 	callback:function (r)
+					// 	{
+					// 		console.log(r.message)
+					// 	}
+					// })
 				}
 			})
 

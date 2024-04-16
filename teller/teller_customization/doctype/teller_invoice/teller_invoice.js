f5d9481d1d8ffb8f49170d32eccf0fc53764d2ac
msabour11 <mohamedabdo2611@gmil.com>
2024-04-16 11:10:50 -0400

    Add Transaction child table to Teller Invoice

--- teller/teller_customization/doctype/teller_invoice/teller_invoice.js
+++ teller/teller_customization/doctype/teller_invoice/teller_invoice.js
@@ -215,7 +215,144 @@ frappe.ui.form.on("Teller Invoice", {
   },
 });
 // set currency items code and selling  rate
+//  Transactions currency table
+frappe.ui.form.on("Entry Child", {
+  paid_from: function (frm, cdt, cdn) {
+    var row = locals[cdt][cdn];
+    if (row.paid_from) {
+      frappe.call({
+        method:
+          "teller.teller_customization.doctype.sales_entry.sales_entry.get_currency",
+        args: {
+          account: row.paid_from,
+        },
+        callback: function (r) {
+          console.log(r.message);
+          console.log(r.message[0]);
+          let curr = r.message[0];
+          let currency_rate = r.message[1];
+          frappe.model.set_value(cdt, cdn, "currency", curr);
+          frappe.model.set_value(cdt, cdn, "rate", currency_rate);
+        },
+      });
+    }
+  },
 
+  // usd_amount: function (frm, cdt, cdn) {
+  //   var row = locals[cdt][cdn];
+  //   // frappe.msgprint("hello")
+
+  //   if (row.paid_from && row.paid_to && row.usd_amount && row.received_amount) {
+  //     let total = row.usd_amount * row.rate;
+  //     console.log(total);
+  //     console.log(frm.doc.comapny);
+  //     frappe.model.set_value(cdt, cdn, "total_amount", total);
+  //     // Create ledger entry
+
+  //     frappe.call({
+  //       method:
+  //         "teller.teller_customization.doctype.sales_entry.sales_entry.get_account_balance",
+  //       args: {
+  //         paid_from: row.paid_from,
+  //         company: frm.doc.comapny,
+  //       },
+  //       callback: function (r) {
+  //         if (r.message) {
+  //           console.log(r.message);
+  //           let account_balance = r.message;
+
+  //           frm.set_value("balance", account_balance);
+  //         } else {
+  //           console.log("not found");
+  //         }
+  //       },
+  //     });
+  //   } else {
+  //     frappe.throw("you must enter this fields");
+  //   }
+  // },
+  usd_amount: function (frm, cdt, cdn) {
+    var row = locals[cdt][cdn];
+
+    if (row.paid_from && row.paid_to && row.usd_amount && row.received_amount) {
+      let total = row.usd_amount * row.rate;
+
+      frappe.model.set_value(cdt, cdn, "total_amount", total);
+
+      // Update account balances
+
+      frappe.call({
+        method:
+          "teller.teller_customization.doctype.sales_entry.sales_entry.get_account_balance",
+        args: {
+          paid_from: row.paid_from,
+          company: frm.doc.comapny,
+        },
+        callback: function (r) {
+          if (r.message) {
+            console.log(r.message);
+            let account_balance = r.message;
+
+            frm.set_value("balance", account_balance);
+          } else {
+            console.log("not found");
+          }
+        },
+      });
+      frappe.call({
+        method:
+          "teller.teller_customization.doctype.sales_entry.sales_entry.paid_to_account_balance",
+        args: {
+          paid_to: row.paid_to,
+          company: frm.doc.comapny,
+        },
+        callback: function (r) {
+          if (r.message) {
+            console.log(r.message);
+            let balance_to = r.message;
+
+            frm.set_value("balance_to", balance_to);
+          } else {
+            console.log("not found");
+          }
+        },
+
+      })
+    } else {
+      frappe.throw("You must enter all required fields.");
+    }
+  },
+  // on_submit: function (frm, cdt, cdn) {
+  //   let row = locals[cdt][cdn];
+  //   if (row.paid_from && row.paid_to && row.usd_amount && row.received_amount) {
+  //     frappe.call({
+  //       method:
+  //         "teller.teller_customization.doctype.sales_entry.sales_entry.create_gl_entry",
+  //       args: {
+  //         account_from: row.paid_from,
+  //         account_to: row.paid_to,
+  //         usd_amount: row.usd_amount,
+  //         credit_amount: row.total_amount,
+  //         currency: row.currency,
+  //         currency_rate: row.rate,
+  //         voucher_no: frm.doc.name,
+  //       },
+  //       callback: function (r) {
+  //         if (r.message) {
+  //           // Update the UI or show a message if needed
+  //           console.log("create gl ");
+  //           console.log(r.message);
+  //           console.log("Account balances updated successfully.");
+  //         } else {
+  //           console.log("Failed to update account balances.");
+  //         }
+  //       },
+  //     });
+  //   }
+  // },
+});
+
+// items price table
 frappe.ui.form.on("Teller Items", {
   item_code: function (frm, cdt, cdn) {
     var row = locals[cdt][cdn];
--- teller/teller_customization/doctype/teller_invoice/teller_invoice.py
+++ teller/teller_customization/doctype/teller_invoice/teller_invoice.py
@@ -3,51 +3,150 @@
 import frappe
 from frappe.model.document import Document
 from frappe.utils import add_days, cint, cstr, flt, formatdate, get_link_to_form, getdate, nowdate
-
+from frappe import get_doc
+from frappe.model.document import Document
+from frappe.utils import nowdate, nowtime
+from erpnext.accounts.utils import (
+    cancel_exchange_gain_loss_journal,
+    get_account_currency,
+    get_balance_on,
+    get_outstanding_invoices,
+    get_party_types_from_account_type,
+)
+from frappe import _, utils
 
 
 class TellerInvoice(Document):
+    def on_submit(self):
+        pass
+
+    def onload(self):
+        self.set_treasury()
+        self.set_branch()
+        self.set_price()
+        self.set_cost()
+        # self.get_active_shift()
+        # self.get_printing_roll()
+
+    def set_treasury(self):
+        treasury_code = frappe.db.get_single_value('Teller Setting', 'treasury_code')
+        self.treasury_code = treasury_code
+
+    def set_branch(self):
+        branch = frappe.db.get_single_value('Teller Setting', 'branch')
+        self.branch_number = branch
+
+    def set_price(self):
+        price_lst = frappe.db.get_single_value('Teller Setting', 'price_list')
+        self.price_list = price_lst
+
+    def set_cost(self):
+        cost = frappe.db.get_single_value('Teller Setting', 'cost_center')
+        self.cost_center_number = cost
+
+    def get_active_shift(self):
+        shift = frappe.db.get_value('OPen Shift', {'active': 1})
+        user = frappe.db.get_value('OPen Shift', {'active': 1}, 'current_user')
+        self.shift = shift
+        self.teller = user
 
 
-	def onload(self):
-		self.set_treasury()
-		self.set_branch()
-		self.set_price()
-		self.set_cost()
-		# self.get_active_shift()
-		# self.get_printing_roll()
 
-	def set_treasury(self):
-		treasury_code = frappe.db.get_single_value('Teller Setting', 'treasury_code')
-		self.treasury_code = treasury_code
-
-	def set_branch(self):
-		branch = frappe.db.get_single_value('Teller Setting', 'branch')
-		self.branch_number = branch
-
-	def set_price(self):
-		price_lst = frappe.db.get_single_value('Teller Setting', 'price_list')
-		self.price_list = price_lst
-
-	def set_cost(self):
-		cost = frappe.db.get_single_value('Teller Setting', 'cost_center')
-		self.cost_center_number = cost
-
-	def get_active_shift(self):
-		shift = frappe.db.get_value('OPen Shift', {'active': 1})
-		user = frappe.db.get_value('OPen Shift', {'active': 1}, 'current_user')
-		self.shift = shift
-		self.teller = user
-
-	# def get_printing_roll(self):
-	# 	roll_code = frappe.db.get_value('Printing Roll',{'active':1},)
-	# 	self.current_roll = roll_code
-	# 	last_printed_no = frappe.db.get_value('Printing Roll',{'active':1},'last_printed_number')
-	# 	lettres = frappe.db.get_value('Printing Roll',{'active':1},'starting_letters')
-	# 	frappe.msgprint(roll_code)
-	#
-	# 	last_printed_no += 1
-	# 	recp=f"{lettres}-{last_printed_no}"
-	# 	self.receipt_number = recp
+    # def get_printing_roll(self):
+    # 	roll_code = frappe.db.get_value('Printing Roll',{'active':1},)
+    # 	self.current_roll = roll_code
+    # 	last_printed_no = frappe.db.get_value('Printing Roll',{'active':1},'last_printed_number')
+    # 	lettres = frappe.db.get_value('Printing Roll',{'active':1},'starting_letters')
+    # 	frappe.msgprint(roll_code)
+    #
+    # 	last_printed_no += 1
+    # 	recp=f"{lettres}-{last_printed_no}"
+    # 	self.receipt_number = recp
 
 
+@frappe.whitelist(allow_guest=True)
+def get_currency(account):
+    # currency = frappe.db.get_value("Payment Entry",{"paid_from":account}, "paid_from_account_currency")
+    currency = frappe.db.get_value("Account", {"name": account}, "account_currency")
+    currency_rate = frappe.db.get_value("Currency Exchange", {"from_currency": currency}, "exchange_rate")
+    return currency, currency_rate
+
+
+@frappe.whitelist(allow_guest=True)
+def get_account_balance(paid_from, company):
+    try:
+        balance = get_balance_on(
+            account=paid_from,
+            company=company,
+            # cost_center=cost_center,
+            # date=date,
+            # party_type=party_type,
+            # party=party,
+            # ignore_account_permission=True
+        )
+        return balance
+    except Exception as e:
+        error_message = f"Error fetching account balance: {str(e)}"
+        frappe.log_error(error_message)
+        return _("Error: Unable to fetch account balance.")  # Return a descriptive error message
+
+
+@frappe.whitelist(allow_guest=True)
+def create_gl_entry(account_from, account_to, usd_amount, currency, currency_rate, voucher_no, credit_amount,
+                    credit_in_transaction_currency):
+    # Create a new GL Entry document
+    gl_entry = get_doc({
+        'doctype': 'GL Entry',
+        'posting_date': nowdate(),  # Use the current date
+        'account': account_from,  # Debit account (From)
+        'against': account_to,  # Credit account (To)
+        'debit': 0,
+        'credit': credit_amount,
+        'credit_in_account_currency': usd_amount,
+        'debit_in_account_currency': 0,  # Credit amount
+        'account_currency': currency,  # Currency
+        'exchange_rate': currency_rate,
+        'voucher_type': 'Sales Entry',  # Currency rate
+        'voucher_no': voucher_no,
+        "credit_in_transaction_currency": credit_in_transaction_currency
+    })
+
+    # Insert the document into the database
+    gl_entry.insert()
+    gl_entry.submit()
+
+    # Create a new GL Entry document for the credit entry
+    credit_gl_entry = get_doc({
+        'doctype': 'GL Entry',
+        'posting_date': nowdate(),  # Use the current date
+        'account': account_to,  # Debit account (To)
+        "debit": credit_amount,
+        "credit": 0,
+        'against_account': account_from,  # Credit account (From)
+        'debit_in_account_currency': credit_amount,  # Debit amount
+        'credit_in_account_currency': 0,  # Credit amount
+        # 'account_currency': currency,  # Currency
+        # 'exchange_rate': currency_rate,  # Currency rate
+        'voucher_type': 'Sales Entry',  # Voucher Type
+        'voucher_no': voucher_no,
+        # Voucher No
+    })
+    credit_gl_entry.insert()
+    credit_gl_entry.submit()
+
+    return gl_entry
+
+
+@frappe.whitelist(allow_guest=True)
+def paid_to_account_balance(paid_to, company):
+    try:
+        balance = get_balance_on(
+            account=paid_to,
+            company=company,
+
+        )
+        return balance
+    except Exception as e:
+        error_message = f"Error fetching account balance: {str(e)}"
+        frappe.log_error(error_message)
+        return _("Error: Unable to fetch account balance.")  # Return a descriptive error message

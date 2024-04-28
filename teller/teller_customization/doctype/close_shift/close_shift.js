4ed1b3bf2e6a610dff196550c04770f3966c6cc1
msabour11 <mohamedabdo2611@gmil.com>
2024-04-28 09:53:06 -0400

    add close shift for Teller invoice

--- teller/teller_customization/doctype/close_shift/close_shift.js
+++ teller/teller_customization/doctype/close_shift/close_shift.js
@@ -2,135 +2,168 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Close Shift", {
-  refresh(frm) {},
-  async get_invoices(frm) {
-    try {
-      let total_purchase_invoices = 0;
-      const response = await frappe.call({
-        method: "frappe.client.get_list",
-        args: {
-          doctype: "Teller Purchase",
-          filters: {
-            shift: frm.doc.open_shift,
-            teller: frm.doc.current_user,
-          },
-        },
-      });
-
-      if (response.message) {
-        const invoices = response.message;
-
-        for (let row of invoices) {
-          const invResponse = await frappe.call({
-            method: "frappe.client.get",
-            args: {
-              doctype: "Teller Purchase",
-              name: row.name,
-            },
-          });
-
-          if (invResponse.message) {
-            const invocies = invResponse.message;
-            const arr = invocies.items;
-            let amount = 0;
-            console.log(arr);
-
-            arr.forEach((element) => {
-              // console.log(element["amount"]);
-              amount += element["amount"];
-              frm.add_child("invoices_tables", {
-                reference: invocies.name,
-                //  total: invocies.total,
-                amount: element["amount"],
-                item_code: element["item_code"],
-                quantity: element["quantity"],
-              });
-            });
-
-            // frm.add_child("invoices_tables", {
-            //   reference: invocies.name,
-            //   total: invocies.total,
-            //   // amount: element["amount"],
-            // });
-
-            total_purchase_invoices += invocies.total;
-          }
-        }
-      }
-
-      frm.set_value("total_purchase", total_purchase_invoices);
-      frm.refresh_field("invoices_tables");
-    } catch (err) {
-      console.error(err);
-    }
-    try {
-      let total_sale_invoices = 0;
-      const response = await frappe.call({
-        method: "frappe.client.get_list",
-        args: {
-          doctype: "Teller Invoice",
-          filters: {
-            shift: frm.doc.open_shift,
-            teller: frm.doc.current_user,
-          },
-        },
-      });
-
-      if (response.message) {
-        const sale_invoices = response.message;
-
-        for (let row of sale_invoices) {
-          const invResponse = await frappe.call({
-            method: "frappe.client.get",
-            args: {
-              doctype: "Teller Invoice",
-              name: row.name,
-            },
-          });
-
-          if (invResponse.message) {
-            const invocies = invResponse.message;
-            const arr = invocies.items;
-            let amount = 0;
-
-            arr.forEach((element) => {
-              console.log(element["amount"]);
-              amount += element["amount"];
-              frm.add_child("sales_table", {
-                reference: invocies.name,
-                //  total: invocies.total,
-                amount: element["amount"],
-                item_code: element["item_code"],
-                quantity: element["quantity"],
-              });
-            });
-
-            // total currency
-            // arr.forEach((element) => {
-            //   console.log(element["amount"]);
-            //   amount += element["amount"];
-            //   frm.add_child("total_currency", {
-            //     amount: element["amount"],
-            //     item_code: element["item_code"],
-            //     quantity: element["quantity"],
-            //   });
-            // });
-
-            // frm.add_child("sales_table", {
-            //   reference: invocies.name,
-            //   total: invocies.total,
-            //   // amount: amount,
-            // });
-
-            total_sale_invoices += invocies.total;
-          }
-        }
-      }
-
-      frm.set_value("total_sales", total_sale_invoices);
-      frm.refresh_field("sales_table");
-    } catch (err) {
-      console.error(err);
-    }
+  refresh(frm) {
+    frappe.call({
+      method:
+        "teller.teller_customization.doctype.close_shift.close_shift.get_active_shift",
+      callback: function (r) {
+        console.log(r.message);
+        frm.set_value("open_shift", r.message);
+      },
+    });
+    frappe.call({
+      method:
+        "teller.teller_customization.doctype.close_shift.close_shift.active_active_user",
+      callback: function (r) {
+        console.log(r.message["name"]);
+        frm.set_value("current_user", r.message["name"]);
+      },
+    });
   },
+
+  get_invoices: function (frm) {
+    console.log(frm.doc.name);
+    frappe.call({
+      method:
+        "teller.teller_customization.doctype.close_shift.close_shift.get_sales_invoice",
+      args: {
+        close_shift_name: frm.doc.name,
+        current_open_shift: frm.doc.open_shift,
+      },
+      callback: function (r) {
+        console.log(r);
+      },
+    });
+  },
+
+  //  async get_invoices(frm) {
+  //    try {
+  //      let total_purchase_invoices = 0;
+  //      const response = await frappe.call({
+  //        method: "frappe.client.get_list",
+  //        args: {
+  //          doctype: "Teller Purchase",
+  //          filters: {
+  //            shift: frm.doc.open_shift,
+  //            teller: frm.doc.current_user,
+  //          },
+  //        },
+  //      });
+  //
+  //      if (response.message) {
+  //        const invoices = response.message;
+  //
+  //        for (let row of invoices) {
+  //          const invResponse = await frappe.call({
+  //            method: "frappe.client.get",
+  //            args: {
+  //              doctype: "Teller Purchase",
+  //              name: row.name,
+  //            },
+  //          });
+  //
+  //          if (invResponse.message) {
+  //            const invocies = invResponse.message;
+  //            const arr = invocies.items;
+  //            let amount = 0;
+  //            console.log(arr);
+  //
+  //            arr.forEach((element) => {
+  //              // console.log(element["amount"]);
+  //              amount += element["amount"];
+  //              frm.add_child("invoices_tables", {
+  //                reference: invocies.name,
+  //                //  total: invocies.total,
+  //                amount: element["amount"],
+  //                item_code: element["item_code"],
+  //                quantity: element["quantity"],
+  //              });
+  //            });
+  //
+  //            // frm.add_child("invoices_tables", {
+  //            //   reference: invocies.name,
+  //            //   total: invocies.total,
+  //            //   // amount: element["amount"],
+  //            // });
+  //
+  //            total_purchase_invoices += invocies.total;
+  //          }
+  //        }
+  //      }
+  //
+  //      frm.set_value("total_purchase", total_purchase_invoices);
+  //      frm.refresh_field("invoices_tables");
+  //    } catch (err) {
+  //      console.error(err);
+  //    }
+  //    try {
+  //      let total_sale_invoices = 0;
+  //      const response = await frappe.call({
+  //        method: "frappe.client.get_list",
+  //        args: {
+  //          doctype: "Teller Invoice",
+  //          filters: {
+  //            shift: frm.doc.open_shift,
+  //            teller: frm.doc.current_user,
+  //          },
+  //        },
+  //      });
+  //
+  //      if (response.message) {
+  //        const sale_invoices = response.message;
+  //
+  //        for (let row of sale_invoices) {
+  //          const invResponse = await frappe.call({
+  //            method: "frappe.client.get",
+  //            args: {
+  //              doctype: "Teller Invoice",
+  //              name: row.name,
+  //            },
+  //          });
+  //
+  //          if (invResponse.message) {
+  //            const invocies = invResponse.message;
+  //            const arr = invocies.items;
+  //            let amount = 0;
+  //
+  //            arr.forEach((element) => {
+  //              console.log(element["amount"]);
+  //              amount += element["amount"];
+  //              frm.add_child("sales_table", {
+  //                reference: invocies.name,
+  //                //  total: invocies.total,
+  //                amount: element["amount"],
+  //                item_code: element["item_code"],
+  //                quantity: element["quantity"],
+  //              });
+  //            });
+  //
+  //            // total currency
+  //            // arr.forEach((element) => {
+  //            //   console.log(element["amount"]);
+  //            //   amount += element["amount"];
+  //            //   frm.add_child("total_currency", {
+  //            //     amount: element["amount"],
+  //            //     item_code: element["item_code"],
+  //            //     quantity: element["quantity"],
+  //            //   });
+  //            // });
+  //
+  //            // frm.add_child("sales_table", {
+  //            //   reference: invocies.name,
+  //            //   total: invocies.total,
+  //            //   // amount: amount,
+  //            // });
+  //
+  //            total_sale_invoices += invocies.total;
+  //          }
+  //        }
+  //      }
+  //
+  //      frm.set_value("total_sales", total_sale_invoices);
+  //      frm.refresh_field("sales_table");
+  //    } catch (err) {
+  //      console.error(err);
+  //    }
+  //  },
 });
--- teller/teller_customization/doctype/close_shift/close_shift.py
+++ teller/teller_customization/doctype/close_shift/close_shift.py
@@ -1,9 +1,171 @@
 # Copyright (c) 2024, Mohamed AbdElsabour and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import whitelist
 
 
 class CloseShift(Document):
-	pass
+    pass
+
+
+# @whitelist(allow_guest=True)
+# def get_sales_invoice(close_shift_name):
+#     test = []
+
+#     child_list = []
+#     invoice_list = []
+
+#     invs = frappe.db.get_list("Teller Invoice", {"docstatus": 1})
+#     for i in invs:
+#         doc = frappe.get_doc("Teller Invoice", i)
+#         invoice_list.append(doc)
+
+#     # # # user = frappe.get_doc("User", frappe.session.user)
+#     # # # active_open_shift = frappe.db.get_list("OPen Shift", {"active": 1})
+
+#     # # # active_open_shift_name = active_open_shift[0]["name"]
+#     parent = frappe.get_doc("Close Shift", close_shift_name)
+
+#     for invoice in invoice_list:
+#         for transaction in invoice.transactions:
+#             existing_child = next(
+#                 (
+#                     c
+#                     for c in child_list
+#                     if c.reference == invoice.name
+#                     and c.currency == transaction.currency
+#                 ),
+#                 None,
+#             )
+#             if existing_child is None:
+#                 child = frappe.new_doc("Sales Table")
+#                 child.update(
+#                     {
+#                         "doctype": "Sales Table",
+#                         "parent": parent.name,
+#                         "parentfield": "sales_table",
+#                         "parenttype": parent.doctype,
+#                         "reference": invoice.name,
+#                         "currency": transaction.currency,
+#                         "currency_amount": transaction.usd_amount,
+#                         "currency_balance": transaction.balance,
+#                         "egy_amount": transaction.total_amount,
+#                     }
+#                 )
+
+#                 parent.append("sales_table", child)
+#                 child_list.append(child)
+
+#         # currency_amount
+#     parent.save()
+#     return child_list
+
+
+##############################3
+# @whitelist()
+# def get_sales_invoice(close_shift_name):
+#     child_list = []
+#     invoice_list = []
+
+#     invs = frappe.db.get_list("Teller Invoice", {"docstatus": 1})
+#     for i in invs:
+#         doc = frappe.get_doc("Teller Invoice", i)
+#         invoice_list.append(doc)
+
+#     parent = frappe.get_doc("Close Shift", close_shift_name)
+
+#     for invoice in invoice_list:
+#         for transaction in invoice.transactions:
+#             existing_child = next(
+#                 (
+#                     c
+#                     for c in parent.sales_table
+#                     if c.reference == invoice.name
+#                     and c.currency == transaction.currency
+#                 ),
+#                 None,
+#             )
+#             if existing_child is None:
+#                 child = frappe.new_doc("Sales Table")
+#                 child.update(
+#                     {
+#                         "doctype": "Sales Table",
+#                         "parent": parent.name,
+#                         "parentfield": "sales_table",
+#                         "parenttype": parent.doctype,
+#                         "reference": invoice.name,
+#                         "currency": transaction.currency,
+#                         "currency_amount": transaction.usd_amount,
+#                         "currency_balance": transaction.balance,
+#                         "egy_amount": transaction.total_amount,
+#                     }
+#                 )
+
+#                 parent.append("sales_table", child)
+#                 child_list.append(child)
+
+#     parent.save()
+#     return child_list
+
+
+@whitelist()
+def active_active_user():
+    user = frappe.get_doc("User", frappe.session.user)
+    return user
+
+
+@whitelist()
+def get_active_shift():
+    active_open_shift = frappe.db.get_list("OPen Shift", {"active": 1})
+
+    active_open_shift_name = active_open_shift[0]["name"]
+    return active_open_shift_name
+
+
+@whitelist()
+def get_sales_invoice(close_shift_name, current_open_shift):
+    child_list = []
+    invoice_list = []
+    currency_list = []
+
+    invs = frappe.db.get_list(
+        "Teller Invoice", {"docstatus": 1, "shift": current_open_shift}
+    )
+    for i in invs:
+        doc = frappe.get_doc("Teller Invoice", i)
+        invoice_list.append(doc)
+
+    parent = frappe.get_doc("Close Shift", close_shift_name)
+
+    for invoice in invoice_list:
+        for transaction in invoice.transactions:
+
+            if not any(
+                c.reference == invoice.name and c.currency == transaction.currency
+                for c in parent.sales_table
+            ):
+                child = frappe.new_doc("Sales Table")
+                child.update(
+                    {
+                        "doctype": "Sales Table",
+                        "parent": parent.name,
+                        "parentfield": "sales_table",
+                        "parenttype": parent.doctype,
+                        "reference": invoice.name,
+                        "currency": transaction.currency,
+                        "currency_amount": transaction.usd_amount,
+                        "currency_balance": transaction.balance,
+                        "egy_amount": transaction.total_amount,
+                    }
+                )
+
+                parent.append("sales_table", child)
+                child_list.append(child)
+    total = 0
+    for c in child_list:
+        currency_list.append(c.currency)
+
+    parent.save()
+    return f"the list is {currency_list}"

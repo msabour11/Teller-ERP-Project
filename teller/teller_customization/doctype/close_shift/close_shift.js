794aa6291a46882037b45944310a67cfa33b3207
msabour11 <mohamedabdo2611@gmil.com>
2024-05-12 06:28:07 -0400

    Handle Retrieve Sales And Purchase Invoices in Close shift in Client Scri

--- teller/teller_customization/doctype/close_shift/close_shift.js
+++ teller/teller_customization/doctype/close_shift/close_shift.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Close Shift", {
-  refresh(frm) {
+  setup(frm) {
     frappe.call({
       method:
         "teller.teller_customization.doctype.close_shift.close_shift.get_active_shift",
@@ -11,6 +11,7 @@ frappe.ui.form.on("Close Shift", {
         frm.set_value("open_shift", r.message);
       },
     });
+
     frappe.call({
       method:
         "teller.teller_customization.doctype.close_shift.close_shift.active_active_user",
@@ -20,256 +21,104 @@ frappe.ui.form.on("Close Shift", {
       },
     });
   },
-
-  // get_invoices: function (frm) {
-  //   console.log(frm.doc.name);
-  //   frappe.call({
-  //     method:
-  //       "teller.teller_customization.doctype.close_shift.close_shift.get_sales_invoice",
-  //     args: {
-  //       close_shift_name: frm.doc.name,
-  //       current_open_shift: frm.doc.open_shift,
-  //     },
-  //     callback: function (r) {
-  //       // console.log(r.message[0]);
-  //       // console.log(r.message[1]);
-  //       // frappe.publish_progress(
-  //       //   25,
-  //       //   (title = "Some title"),
-  //       //   (description = "Some description")
-  //       // );
-  //       // frappe.publish_progress(100, "from JS");
-  //       console.log("total is ", r.message[0]);
-  //       let total = r.message[0];
-  //       console.log("type is ", typeof total);
-  //       frm.set_value("total_sales", total);
-
-  //       // Calculate progress based on array length
-  //       let arr = r.message[1];
-
-  //       let progressStep = 100 / arr.length;
-  //       let currentProgress = 0;
-
-  //       for (let i = 0; i < arr.length; i++) {
-  //         // Simulate processing time for each item
-  //         setTimeout(() => {
-  //           currentProgress += progressStep;
-  //           frappe.show_progress(
-  //             "Getting Invoices...",
-  //             currentProgress,
-  //             "Please wait"
-  //           );
-  //         }, 500 * i); // Adjust delay as needed
-  //       }
-
-  //       // let arr = r.message[1];
-  //       // console.log("arr is", arr);
-  //       // for (let i = 0; i < arr.length; i++) {
-  //       //   frappe.show_progress(
-  //       //     "Getting Contacts..",
-  //       //     (i + 1) * 10,
-  //       //     "Please wait"
-  //       //   );
-  //       // }
-
-  //       // frappe.show_progress("Getting Contacts..", 90, 100, "Please wait");
-  //       // frappe.hide_progress();
-  //     },
-  //     // freeze: true,
-  //     // freeze_message: "Processing data...",
-  //     // progress: function (progress) {
-  //     //   frappe.show_progress("Processing data", 90, 100, "wait");
-  //     // },
-  //   });
-  // },
-  get_invoices: function (frm) {
-    console.log(frm.doc.name);
+  serial: (frm) => {
     frappe.call({
-      method:
-        "teller.teller_customization.doctype.close_shift.close_shift.get_sales_invoice",
-      args: {
-        close_shift_name: frm.doc.name,
-        current_open_shift: frm.doc.open_shift,
-      },
-      callback: function (r) {
-        console.log("total is ", r.message[0]);
-        let total = r.message[0];
-        console.log("type is ", typeof total);
-        frm.set_value("total_sales", total);
-
-        // Calculate progress based on array length
-        let arr = r.message[1];
-
-        let progressStep = 100 / arr.length;
-        let currentProgress = 0;
-
-        for (let i = 0; i < arr.length; i++) {
-          // Simulate processing time for each item
-          setTimeout(() => {
-            currentProgress += progressStep;
-            frappe.show_progress(
-              "Getting Invoices...",
-              currentProgress,
-              "Please wait"
-            );
-          }, 500 * i); // Adjust delay as needed
-        }
-
-        // Hide the progress bar after all invoices have been processed
-        setTimeout(() => {
-          frappe.hide_progress();
-        }, 500 * arr.length);
+      doc: frm.doc,
+      method: "call_from_class",
+      callback: (r) => {
+        console.log(r.message);
       },
     });
   },
 
-  // on_save: function (frm) {
-  //   frappe.call({
-  //     method:
-  //       "teller.teller_customization.doctype.close_shift.close_shift.get_sales_invoice",
-  //     args: {
-  //       close_shift_name: frm.doc.name,
-  //       current_open_shift: frm.doc.open_shift,
-  //     },
-  //     callback: function (r) {
-  //       console.log(r.message[0]);
-  //       console.log("total is ", r.message[1]);
-  //       let total = r.message[1];
-  //       console.log("type is ", typeof total);
+  get_invoices: function (frm) {
+    console.log(frm.doc.name);
 
-  //       frm.set_value("total_sales", total);
-  //     },
-  //   });
-  // },
+    frappe
+      .call({
+        method:
+          "teller.teller_customization.doctype.close_shift.close_shift.get_sales_invoice",
+        args: {
+          current_open_shift: frm.doc.open_shift,
+        },
+      })
+      .then((r) => {
+        let total = 0;
 
-  //  async get_invoices(frm) {
-  //    try {
-  //      let total_purchase_invoices = 0;
-  //      const response = await frappe.call({
-  //        method: "frappe.client.get_list",
-  //        args: {
-  //          doctype: "Teller Purchase",
-  //          filters: {
-  //            shift: frm.doc.open_shift,
-  //            teller: frm.doc.current_user,
-  //          },
-  //        },
-  //      });
-  //
-  //      if (response.message) {
-  //        const invoices = response.message;
-  //
-  //        for (let row of invoices) {
-  //          const invResponse = await frappe.call({
-  //            method: "frappe.client.get",
-  //            args: {
-  //              doctype: "Teller Purchase",
-  //              name: row.name,
-  //            },
-  //          });
-  //
-  //          if (invResponse.message) {
-  //            const invocies = invResponse.message;
-  //            const arr = invocies.items;
-  //            let amount = 0;
-  //            console.log(arr);
-  //
-  //            arr.forEach((element) => {
-  //              // console.log(element["amount"]);
-  //              amount += element["amount"];
-  //              frm.add_child("invoices_tables", {
-  //                reference: invocies.name,
-  //                //  total: invocies.total,
-  //                amount: element["amount"],
-  //                item_code: element["item_code"],
-  //                quantity: element["quantity"],
-  //              });
-  //            });
-  //
-  //            // frm.add_child("invoices_tables", {
-  //            //   reference: invocies.name,
-  //            //   total: invocies.total,
-  //            //   // amount: element["amount"],
-  //            // });
-  //
-  //            total_purchase_invoices += invocies.total;
-  //          }
-  //        }
-  //      }
-  //
-  //      frm.set_value("total_purchase", total_purchase_invoices);
-  //      frm.refresh_field("invoices_tables");
-  //    } catch (err) {
-  //      console.error(err);
-  //    }
-  //    try {
-  //      let total_sale_invoices = 0;
-  //      const response = await frappe.call({
-  //        method: "frappe.client.get_list",
-  //        args: {
-  //          doctype: "Teller Invoice",
-  //          filters: {
-  //            shift: frm.doc.open_shift,
-  //            teller: frm.doc.current_user,
-  //          },
-  //        },
-  //      });
-  //
-  //      if (response.message) {
-  //        const sale_invoices = response.message;
-  //
-  //        for (let row of sale_invoices) {
-  //          const invResponse = await frappe.call({
-  //            method: "frappe.client.get",
-  //            args: {
-  //              doctype: "Teller Invoice",
-  //              name: row.name,
-  //            },
-  //          });
-  //
-  //          if (invResponse.message) {
-  //            const invocies = invResponse.message;
-  //            const arr = invocies.items;
-  //            let amount = 0;
-  //
-  //            arr.forEach((element) => {
-  //              console.log(element["amount"]);
-  //              amount += element["amount"];
-  //              frm.add_child("sales_table", {
-  //                reference: invocies.name,
-  //                //  total: invocies.total,
-  //                amount: element["amount"],
-  //                item_code: element["item_code"],
-  //                quantity: element["quantity"],
-  //              });
-  //            });
-  //
-  //            // total currency
-  //            // arr.forEach((element) => {
-  //            //   console.log(element["amount"]);
-  //            //   amount += element["amount"];
-  //            //   frm.add_child("total_currency", {
-  //            //     amount: element["amount"],
-  //            //     item_code: element["item_code"],
-  //            //     quantity: element["quantity"],
-  //            //   });
-  //            // });
-  //
-  //            // frm.add_child("sales_table", {
-  //            //   reference: invocies.name,
-  //            //   total: invocies.total,
-  //            //   // amount: amount,
-  //            // });
-  //
-  //            total_sale_invoices += invocies.total;
-  //          }
-  //        }
-  //      }
-  //
-  //      frm.set_value("total_sales", total_sale_invoices);
-  //      frm.refresh_field("sales_table");
-  //    } catch (err) {
-  //      console.error(err);
-  //    }
-  //  },
+        if (r.message) {
+          console.log(r.message);
+          let invoices = r.message;
+          invoices.forEach((invoice) => {
+            let exists = frm.doc.sales_invoice.some((d) => {
+              return d.reference === invoice.name;
+            });
+            if (!exists) {
+              frm.add_child("sales_invoice", {
+                reference: invoice.name,
+                total: invoice.total,
+                current_roll: invoice.current_roll,
+                date: invoice.date,
+                receipt_number: invoice.receipt_number,
+              });
+            }
+            total += invoice.total;
+            frm.refresh_field("sales_invoice");
+          });
+          frm.set_value("total_sales", total);
+        }
+
+        //////////////////
+      });
+  },
+
+  get_purchase: (frm) => {
+    // console.log("from purchase");
+    frappe.call({
+      method:
+        "teller.teller_customization.doctype.close_shift.close_shift.get_purchase_invoices",
+      args: {
+        current_open_shift: frm.doc.open_shift,
+      },
+      callback: (r) => {
+        let log = console.log;
+
+        const invocies = r.message;
+        const invocie_names = [];
+        console.log(r.message);
+        let egy_total = 0;
+        let child_total = 0;
+
+        for (let invocie of invocies) {
+          invocie_names.push(invocie["name"]);
+          let exists = frm.doc.purchase_close_table.some((d) => {
+            return d.reference === invocie.name;
+          });
+          if (!exists) {
+            for (let child of invocie["transactions"]) {
+              frm.add_child("purchase_close_table", {
+                reference: invocie["name"],
+                currency_amount: child["usd_amount"],
+                currency: child["currency"],
+                egyptian_price: child["total_amount"],
+                rate: child["rate"],
+              });
+              child_total += child["total_amount"];
+            }
+          }
+
+          frm.refresh_field("purchase_close_table");
+        }
+
+        egy_total += child_total;
+
+        log(egy_total);
+        frm.set_value("total_purchase", egy_total);
+        frm.refresh_field("total_purchase");
+
+        log("names are", invocie_names);
+      },
+    });
+  },
+
+  
 });

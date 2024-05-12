1bab23fbb8341030a1c4a1439569be207eb118d8
msabour11 <mohamedabdo2611@gmil.com>
2024-05-12 06:25:24 -0400

    Handle Retrieve Sales And Purchase Invoices in Close shift Back-end file

--- teller/teller_customization/doctype/close_shift/close_shift.py
+++ teller/teller_customization/doctype/close_shift/close_shift.py
@@ -4,110 +4,16 @@
 import frappe
 from frappe.model.document import Document
 from frappe import whitelist
+import time
 
 
 class CloseShift(Document):
-    pass
+    @whitelist()
+    def call_from_class(self):
+        return self.current_user, len(self.sales_table)
 
 
-# @whitelist(allow_guest=True)
-# def get_sales_invoice(close_shift_name):
-#     test = []
-
-#     child_list = []
-#     invoice_list = []
-
-#     invs = frappe.db.get_list("Teller Invoice", {"docstatus": 1})
-#     for i in invs:
-#         doc = frappe.get_doc("Teller Invoice", i)
-#         invoice_list.append(doc)
-
-#     # # # user = frappe.get_doc("User", frappe.session.user)
-#     # # # active_open_shift = frappe.db.get_list("OPen Shift", {"active": 1})
-
-#     # # # active_open_shift_name = active_open_shift[0]["name"]
-#     parent = frappe.get_doc("Close Shift", close_shift_name)
-
-#     for invoice in invoice_list:
-#         for transaction in invoice.transactions:
-#             existing_child = next(
-#                 (
-#                     c
-#                     for c in child_list
-#                     if c.reference == invoice.name
-#                     and c.currency == transaction.currency
-#                 ),
-#                 None,
-#             )
-#             if existing_child is None:
-#                 child = frappe.new_doc("Sales Table")
-#                 child.update(
-#                     {
-#                         "doctype": "Sales Table",
-#                         "parent": parent.name,
-#                         "parentfield": "sales_table",
-#                         "parenttype": parent.doctype,
-#                         "reference": invoice.name,
-#                         "currency": transaction.currency,
-#                         "currency_amount": transaction.usd_amount,
-#                         "currency_balance": transaction.balance,
-#                         "egy_amount": transaction.total_amount,
-#                     }
-#                 )
-
-#                 parent.append("sales_table", child)
-#                 child_list.append(child)
-
-#         # currency_amount
-#     parent.save()
-#     return child_list
-
-
-##############################3
-# @whitelist()
-# def get_sales_invoice(close_shift_name):
-#     child_list = []
-#     invoice_list = []
-
-#     invs = frappe.db.get_list("Teller Invoice", {"docstatus": 1})
-#     for i in invs:
-#         doc = frappe.get_doc("Teller Invoice", i)
-#         invoice_list.append(doc)
-
-#     parent = frappe.get_doc("Close Shift", close_shift_name)
-
-#     for invoice in invoice_list:
-#         for transaction in invoice.transactions:
-#             existing_child = next(
-#                 (
-#                     c
-#                     for c in parent.sales_table
-#                     if c.reference == invoice.name
-#                     and c.currency == transaction.currency
-#                 ),
-#                 None,
-#             )
-#             if existing_child is None:
-#                 child = frappe.new_doc("Sales Table")
-#                 child.update(
-#                     {
-#                         "doctype": "Sales Table",
-#                         "parent": parent.name,
-#                         "parentfield": "sales_table",
-#                         "parenttype": parent.doctype,
-#                         "reference": invoice.name,
-#                         "currency": transaction.currency,
-#                         "currency_amount": transaction.usd_amount,
-#                         "currency_balance": transaction.balance,
-#                         "egy_amount": transaction.total_amount,
-#                     }
-#                 )
-
-#                 parent.append("sales_table", child)
-#                 child_list.append(child)
-
-#     parent.save()
-#     return child_list
+##############################
 
 
 @whitelist()
@@ -125,47 +31,30 @@ def get_active_shift():
 
 
 @whitelist()
-def get_sales_invoice(close_shift_name, current_open_shift):
-    total = 0
-    child_list = []
-    invoice_list = []
-    currency_list = []
+def get_purchase_invoices(current_open_shift):
 
-    invs = frappe.db.get_list(
-        "Teller Invoice", {"docstatus": 1, "shift": current_open_shift}
+    invoice_list = []
+
+    invoice_names = frappe.db.get_list(
+        "Teller Purchase", {"docstatus": 1, "shift": current_open_shift}
     )
-    for i in invs:
-        doc = frappe.get_doc("Teller Invoice", i)
+    for i in invoice_names:
+        doc = frappe.get_doc("Teller Purchase", i)
         invoice_list.append(doc)
 
-    parent = frappe.get_doc("Close Shift", close_shift_name)
+    return invoice_list
 
-    for invoice in invoice_list:
-        for transaction in invoice.transactions:
 
-            if not any(
-                c.reference == invoice.name and c.currency == transaction.currency
-                for c in parent.sales_table
-            ):
-                child = frappe.new_doc("Sales Table")
-                child.update(
-                    {
-                        "doctype": "Sales Table",
-                        "parent": parent.name,
-                        "parentfield": "sales_table",
-                        "parenttype": parent.doctype,
-                        "reference": invoice.name,
-                        "currency": transaction.currency,
-                        "currency_amount": transaction.usd_amount,
-                        "currency_balance": transaction.balance,
-                        "egy_amount": transaction.total_amount,
-                    }
-                )
+@whitelist(allow_guest=True)
+def get_sales_invoice(current_open_shift):
 
-                parent.append("sales_table", child)
-                child_list.append(child)
-                if transaction.total_amount:
-                    total += transaction.total_amount
+    invoices = []
+    invoice_names = frappe.db.get_all(
+        "Teller Invoice", {"docstatus": 1, "shift": current_open_shift}
+    )
 
-    parent.save()
-    return total, child_list
+    for invoice in invoice_names:
+        doc = frappe.get_doc("Teller Invoice", invoice)
+        invoices.append(doc)
+
+    return invoices

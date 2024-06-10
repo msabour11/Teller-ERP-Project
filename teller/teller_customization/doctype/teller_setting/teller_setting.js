326abff2aff7573d1e4bc2747f2b977d850c51d9
mohamed abdelsabour <83145597+msabour11@users.noreply.github.com>
2024-06-10 05:35:00 -0400

    refacor:add allowed amount and currency rate

--- teller/teller_customization/doctype/teller_setting/teller_setting.js
+++ teller/teller_customization/doctype/teller_setting/teller_setting.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2024, Mohamed AbdElsabour and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Teller Setting", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Teller Setting", {
+  refresh(frm) {
+    console.log(typeof frm.doc.duration);
+    if (frm.doc.currency && frm.doc.amount) {
+      frappe.call({
+        method:
+          "teller.teller_customization.doctype.teller_setting.teller_setting.rate_settings",
+        args: {
+          currency: frm.doc.currency,
+        },
+        callback: function (r) {
+          if (r.message) {
+            console.log(r.message);
+            let allowedAmount = r.message * frm.doc.amount;
+            frm.set_value("allowed_amount", allowedAmount);
+          }
+        },
+      });
+    }
+  },
+});
--- teller/teller_customization/doctype/teller_setting/teller_setting.py
+++ teller/teller_customization/doctype/teller_setting/teller_setting.py
@@ -3,12 +3,21 @@
 
 # import frappe
 from frappe.model.document import Document
+import frappe
 
 
 class TellerSetting(Document):
-	# treasury = frappe.get_single_value("Teller Setting", "treasury_code")
-	# frappe.db.set_value('Teller Purchase','treasury', treasury)
+    # treasury = frappe.get_single_value("Teller Setting", "treasury_code")
+    # frappe.db.set_value('Teller Purchase','treasury', treasury)
 
-	# print(treasury)
-	pass
-	
+    # print(treasury)
+    pass
+
+
+@frappe.whitelist(allow_guest=True)
+def rate_settings(currency):
+    selling_rate = frappe.db.get_value(
+        "Currency Exchange", {"from_currency": currency}, "custom_selling_exchange_rate"
+    )
+
+    return selling_rate

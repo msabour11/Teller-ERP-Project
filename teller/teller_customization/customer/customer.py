e0a924564ce45b8e04ecc1c2860168b6ae91fa59
mohamed abdelsabour <83145597+msabour11@users.noreply.github.com>
2024-08-05 03:36:04 -0400

    modify customer naming

--- teller/teller_customization/customer/customer.py
+++ teller/teller_customization/customer/customer.py
@@ -6,9 +6,11 @@ def autoname(doc, method):
         doc.name = doc.custom_national_id
     elif doc.custom_type == "Company" and doc.custom_commercial_no:
         doc.name = doc.custom_commercial_no
-    elif doc.custom_type == "Foreigner" and doc.custom_passport_number:
+    elif( doc.custom_type == "Foreigner" or doc.custom_type=="Egyptian") and doc.custom_passport_number:
         doc.name = doc.custom_passport_number
     else:
-        frappe.throw(
-            "Naming Error: Please ensure National ID or Commercial No is passport no set properly based on the identity type."
-        )
+        # frappe.throw(
+        #     "Naming Error: Please ensure National ID or Commercial No is passport no set properly based on the identity type."
+        # )
+        doc.name = doc.customer_name
+
